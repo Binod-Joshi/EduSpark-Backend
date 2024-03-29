49d526d346184e7618e0a2df5b6b1ce1119e9cbe
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-03-29 10:21:02 +0530

    subject schema updated

--- models/subjectModel/subjectSchema.js
+++ models/subjectModel/subjectSchema.js
@@ -10,7 +10,9 @@ const SubjectSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Branch',
         required: true
-    }
+    },
+    institute:{type:mongoose.Schema.Types.ObjectId, ref: "Institute",required:true},
+    subjectType:{type:String,enums:['School','Jr College','College'],required:true},
 })
 
 module.exports = mongoose.model('Subject', SubjectSchema);
